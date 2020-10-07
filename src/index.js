4d3d3be4a09c763af7e669b45ccaf53c5327b9a4
aarsha-j <aarsha.joshi@csedge.org>
2020-10-06 23:43:33 -0400

    increment/decrement button(HTML not linked)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-const jsApp = document.getElementById('js-app');
-jsApp.innerHTML = '<h1>Hello from the DOM!</h1>';
-console.log('Hello from the console!');
+// const jsApp = document.getElementById('js-app');
+// jsApp.innerHTML = '<h1>Hello from the DOM!</h1>';
+// console.log('Hello from the console!');
+
+const sum = (a) => (b) => a+b
+// const increment => sum(1)
+
+console.log(increment(1))
+
+const countSpan = document.getElementById('count');
+
+countSpan.innerText = 42;
+let count = 0;
+
+const incrementButton = document.getElementById('increment');
+
+incrementButton.onclick = () => {
+  count++
+}
